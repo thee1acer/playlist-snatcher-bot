aab8c50446902789b6b9ef410f02430f6c866929
Mpho Nkala <mphonkala.info@gmail.com>
2025-03-07 18:38:45 +0200

    try with a larger file

--- api/hello.ts
+++ api/hello.ts
@@ -89,22 +89,39 @@ bot.on("text", async (ctx) => {
         //const outputFolder = `${tempDir}/playlist-${randomUUID()}`;
 
         try {
-          const response = await fetch(
-            //"https://9dbsrzxknugap8tb.public.blob.vercel-storage.com/downloads/apktool-XxfgjqNnldK277fbdlI7H4OOb7qvRo.zip"
-            "https://9dbsrzxknugap8tb.public.blob.vercel-storage.com/downloads/blob-BSiLYXepyTmRXzsivYKBm7r8pIpMwZ.txt"
-          );
+          const signedURL =
+            //"https://9dbsrzxknugap8tb.public.blob.vercel-storage.com/downloads/blob-BSiLYXepyTmRXzsivYKBm7r8pIpMwZ.txt";
+            "https://9dbsrzxknugap8tb.public.blob.vercel-storage.com/downloads/apktool-XxfgjqNnldK277fbdlI7H4OOb7qvRo.zip";
+
+          const response = await fetch(signedURL);
 
           if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
           }
 
-          const fileBuffer = Buffer.from(await response.arrayBuffer());
-          const fileStream = Readable.from(fileBuffer);
+          const contentLength = response.headers.get("content-length");
+          const fileSizeMB = contentLength
+            ? parseInt(contentLength, 10) / (1024 * 1024)
+            : 0;
 
-          await ctx.replyWithDocument(
-            { source: fileStream, filename: "apktool.zip" },
-            { caption: "Here is your file 📂" }
-          );
+          if (fileSizeMB > 50) {
+            ctx.reply(
+              `File size is greater than 50mbs. Here is your download link: ${signedURL}`
+            );
+          } else {
+            const fileBuffer = Buffer.from(await response.arrayBuffer());
+            const fileStream = Readable.from(fileBuffer);
+
+            const fileExtension = signedURL.split(".").pop();
+
+            await ctx.replyWithDocument(
+              {
+                source: fileStream,
+                filename: `file.${fileExtension}`
+              },
+              { caption: "Here is your file 📂" }
+            );
+          }
         } catch (err) {
           console.error("Failed to fetch file:", err);
           await ctx.reply("❌ Failed to send the file.");
